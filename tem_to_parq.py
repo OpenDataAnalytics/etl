0b45e81bdbe543e045edf05f29290b0aef84fc1a
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2017-04-06 13:50:44 -0400

    Wrapped long lines

--- tem_to_parq.py
+++ tem_to_parq.py
@@ -6,7 +6,7 @@ im = io.imread('TiltSeries_NanoParticle_doi_10.1021-nl103400a.tif')
 imgdata2d = im.reshape(im.shape[0] * im.shape[1], im.shape[2])
 index = range(0, im.shape[0] * im.shape[1])
 
-df = pd.DataFrame(imgdata2d.astype(float), columns=[str(i) for i in range(0, 256)], index=index)
+df = pd.DataFrame(imgdata2d.astype(float),
+                  columns=[str(i) for i in range(0, 256)],
+                  index=index)
 fp.write('out.parq', df, compression='GZIP')
-
-
