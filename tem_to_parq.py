7d252ecad97f05d13e2af6a485772cae85c11ff9
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2017-04-08 08:21:24 -0400

    Saving data as int32 for Spark
    To read data in Spark, one needs int32 or int64

--- tem_to_parq.py
+++ tem_to_parq.py
@@ -1,12 +1,28 @@
+# Imports
 from skimage import io
+
 import pandas as pd
+
 import fastparquet as fp
 
-im = io.imread('TiltSeries_NanoParticle_doi_10.1021-nl103400a.tif')
+import numpy as np
+
+import os
+
+# Read file from the disk
+filename = 'TiltSeries_NanoParticle_doi_10.1021-nl103400a.tif'
+filenamePrefix = os.path.splitext(os.path.basename(filename))[0]
+
+im = io.imread(filename)
+
+# Reshape 3D to one giant 2D
 imgdata2d = im.reshape(im.shape[0] * im.shape[1], im.shape[2])
 index = range(0, im.shape[0] * im.shape[1])
 
-df = pd.DataFrame(imgdata2d.astype(float),
+# Convert to Pandas dataframe
+df = pd.DataFrame(imgdata2d.astype(np.int32),
                   columns=[str(i) for i in range(0, 256)],
                   index=index)
-fp.write('out.parq', df, compression='GZIP')
+
+# Write it out as Parquet format
+fp.write(''.join([filenamePrefix, '.parq']), df, compression='GZIP')
